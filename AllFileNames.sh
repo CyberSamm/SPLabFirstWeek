bf20dd358d371f57f770c7e069793364a785b35e
CyberSamm <samvel.mkrtchyan.proff.acc@gmail.com>
2021-10-09 06:54:38 +0400

    Add AllFileNames.sh sctipt, that prints all file names on given folder with subfolders.

---
+++ AllFileNames.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Print all file names on given folder with subfolders
+
+traverse() {
+	for file in $1/*; do
+		echo $file
+
+		if [ -d $file ]; then
+			#if a directory is empty no need to traverse it.
+			if [ -z "$(ls $file)" ]; then 
+				continue
+			else
+				traverse $file
+			fi
+		fi
+	done	
+}
+
+if [ -z $1 ]; then
+	echo "No Parameters found."
+elif [ -e $1 ]; then
+	traverse $1
+else
+	echo "No file with the such name."
+fi
